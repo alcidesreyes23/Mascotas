788e4c1f64e76ee93c30d6b14ee3c9325894fdf8
erick23r <79947181+erick23r@users.noreply.github.com>
2021-04-12 15:36:34 -0600

    Initial commit

---
+++ app/src/main/java/com/example/mascotas/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.mascotas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mostrar = new Intent(MainActivity.this,Menu.class);
+                startActivity(mostrar);
+                finish();
+            }
+        }, 4300);
+    }
+}
